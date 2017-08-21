2d64ce65997468b0b43756b8e325d11320951a3f
Kalid <kalid.yunus@chalmers.se>
2017-08-21 15:23:16 +0200

    PEF calculation for DH corrected

--- fsave_figure.m
+++ fsave_figure.m
@@ -4,5 +4,6 @@ function [ output_args ] = fsave_figure(folder_name,fig_name)
 saveas(gcf,[folder_name fig_name '.fig'])                        %save as .fig
 print('-dpdf','-r300','-painters',[folder_name fig_name '.pdf']) %save as .pdf
 print('-dmeta','-painters',[folder_name fig_name '.emf'])        %save as .emf
+saveas(gcf,[folder_name fig_name '.png'])        %save as .png
 end
 
