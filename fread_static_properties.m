463d815d2401cf39d8f3c55e5511f6cbffb9d202
Kalid <kalid.yunus@chalmers.se>
2018-05-31 15:35:03 +0200

    Dispatch model update
    -The GAMS file 'FED_GENERATE_GDX_FILE' is remove; the section of the code related to PV in this file is moved to 'FED_GET_GDX_FILE'
    
    -FEG_GET_GDX_FILE is updated to compensate the removed GDX file
    
    -Building IDs related PV are now imported from MATLAB;
    
    -FED_MATLAB_MAIN is updated where data related with PV can now be input from MATLAB
    
    -Measurement data reading is updated where the measurement is taken from t-10 to t-1, assuming 10 hour forcast horizon
    
    -The solar PV data (irradiance and area) can be read from MATLAB, which could be used for future use
    
    -More output parameters are added to fstore_results, more can be added to the function
    
    -gdx2mat function is updated to be able to process one and two dimenssional data
    
    -In plots_results, an attemp has been made to relate the PV BID to the buildings, this need to be checked; not 100% sure

--- fread_static_properties.m
+++ fread_static_properties.m
@@ -1,4 +1,4 @@
-function [ area, BTES_param ] = fread_static_properties()
+function [ area_roof,area_facades, BTES_param ] = fread_static_properties()
 %This function reads static properties of model input parameters
 
 irradiance_area_range='A2:AI2';
@@ -7,15 +7,18 @@ BITES_par_range='B2:AJ10';
 %Solar PV area***************************TO BE FIXED
 sheet=2;
 xlRange = irradiance_area_range;
-area=xlsread('Input_dispatch_model\measured_irradiance.xlsx',sheet,xlRange);
-area(isnan(area))=0;
-    
+area_roof=xlsread('Input_dispatch_model\irradianceRoofs.xlsx',sheet,xlRange);
+area_roof(isnan(area_roof))=0;
+
+sheet=2;
+xlRange = irradiance_area_range;
+area_Facades=xlsread('Input_dispatch_model\irradianceFacades.xlsx',sheet,xlRange);
+area_facades(isnan(area_Facades))=0;
+
 %BTES parameters
 sheet=2;
 xlRange = BITES_par_range;
 BTES_param=xlsread('Input_dispatch_model\UFO_TES.xlsx',sheet,xlRange);
 BTES_param(isnan(BTES_param))=0;
-
-
 end
 
