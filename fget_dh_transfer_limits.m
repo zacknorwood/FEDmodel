5ba2f027c7ae7ed804c4a65f0bdc52549599324d
Alexander <24574245+SobertKaos@users.noreply.github.com>
2018-06-04 16:25:35 +0200

    Continued work on get_dh_transfer_limits

--- fget_dh_transfer_limits.m
+++ fget_dh_transfer_limits.m
@@ -5,10 +5,14 @@ if nargin() == 3
 end
 
 
-delta_T_DH = [20 , 20, 20, 20, 25]' % delta T for DH water during h_sim hours
-delta_T_current_hours = 10 .* tout.val
-% Populate delta_T_current_hours with correct temperature difference for
-% coming hours
+% Delta temperatures on format 'outside temperature', 'supply temp - return
+% temp'
+delta_T_DH = {-15, 40;
+              -5, 40;
+              0, 40;
+              10, 30;
+              20, 30
+              };
 
 
 CP_Water = 4.187; % kJ/kgK
@@ -21,8 +25,12 @@ DH.transfer_limits = zeros(length(DH.uels), length(DH.nodes)) % Each column is a
 for node = 1:length(DH.nodes)
     maximum_flow_rate = DH_Nodes.maximum_flow(node);
     for hour = 1:length(DH.uels)
+        % Get current temperature diff
+        [min_temp_diff, index_current_temp] = min(abs([delta_T_DH{1:5,1}]-tout(hour)));
+        delta_T_current = delta_T_DH{index_current_temp, 2};
+
         % Transfer limit [MWh/h] = m3/s * kg/m3 * kJ/kgK  * MW/kW 
-        DH.transfer_limits(hour,node) = maximum_flow_rate * Rho_Water * CP_Water * delta_T_current_hours(hour) * MW_per_kW;
+        DH.transfer_limits(hour,node) = maximum_flow_rate * Rho_Water * CP_Water * delta_T_current * MW_per_kW;
     end
 end
 DH_transfer_limits = DH;
