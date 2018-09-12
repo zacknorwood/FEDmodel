0eda7e185898ed6bfcaa79d0589ea66fd2f1a560
Alexander <24574245+SobertKaos@users.noreply.github.com>
2018-09-12 16:31:54 +0200

    Fix Error in DC Node definition

--- FED_MAT_MAIN.m
+++ FED_MAT_MAIN.m
@@ -356,7 +356,7 @@ DC_Nodes.name = {DC_Node_VoV.name, DC_Node_Maskin.name, DC_Node_EDIT.name, DC_No
 DC_Nodes.maximum_flow = [26, 44, 32, 34, 32] .* 1/1000; % % l/s * m3/l = m3/s which is assumed input by fget_dc_transfer_limits below
 
 DC_Node_ID.name = 'DH_NodeID';
-DC_Nodes.uels = {DC_Node_VoV.name, DC_Node_Maskin.name, DC_Node_EDIT.name, DC_Node_Fysik.name, DC_Node_Kemi.name};
+DC_Node_ID.uels = {DC_Node_VoV.name, DC_Node_Maskin.name, DC_Node_EDIT.name, DC_Node_Fysik.name, DC_Node_Kemi.name};
 
 %Forcasted solar PV irradiance -roof
 G_roof = struct('name','G_roof','type','parameter');
