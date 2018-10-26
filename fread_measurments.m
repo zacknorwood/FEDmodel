0faae8672213367a38057e2d4211465b639ee5f7
Kalid <kalid.yunus@chalmers.se>
2018-10-26 15:10:58 +0200

    Changes in fread_measurments
    -historical data for the local generating units are added to the file

--- fread_measurments.m
+++ fread_measurments.m
@@ -1,5 +1,7 @@
 function [e_demand_measured, h_demand_measured,c_demand_measured,...
-          h_B1_measured,h_F1_measured,e_price_measured,...
+          h_B1_measured,h_F1_measured,...
+          el_VKA1_measured,el_VKA4_measured,el_AAC_measured, h_AbsC_measured,...
+          e_price_measured,...
           el_cirtificate,h_price_measured,tout_measured,...
           irradiance_measured_roof,irradiance_measured_facades] = fread_measurments(t_init, t_len)
 %This function is used to read mesurment between the indicated indices
@@ -39,6 +41,35 @@ sheet=4;
 xlRange = gen_range;
 h_F1_measured=xlsread('Input_dispatch_model\Panna1 2016-2017.xls',sheet,xlRange)*1000;
 h_F1_measured(isnan(h_F1_measured))=0;
+
+%Measured el input for VKA1
+sheet=2;
+xlRange = strcat('C',int2str(2+t_init),':C',int2str(2+t_len+t_init-1));
+h_VKA1_measured=xlsread('Input_dispatch_model\värmepump VKA1.xls',sheet,xlRange);
+h_VKA1_measured(isnan(h_VKA1_measured))=0;
+el_VKA1_measured=h_VKA1_measured/3;
+
+%Measured el input for VKA4
+sheet=2;
+xlRange = strcat('C',int2str(2+t_init),':C',int2str(2+t_len+t_init-1));
+h_VKA4_measured=xlsread('Input_dispatch_model\värmepump VKA4.xls',sheet,xlRange);
+h_VKA4_measured(isnan(h_VKA4_measured))=0;
+el_VKA4_measured=h_VKA4_measured/3;
+
+%Measured el input for AAC
+sheet=2;
+xlRange = strcat('C',int2str(t_init),':C',int2str(t_len+t_init-1));
+c_AAC_measured=xlsread('Input_dispatch_model\abs o frikyla 2016-2017.xlsx',sheet,xlRange)*1000;
+c_AAC_measured(isnan(c_AAC_measured))=0;
+el_AAC_measured=c_AAC_measured/10;
+
+%Measured heat input for AbsC
+sheet=2;
+xlRange = strcat('D',int2str(t_init),':D',int2str(t_len+t_init-1));
+c_AbsC_measured=xlsread('Input_dispatch_model\abs o frikyla 2016-2017.xlsx',sheet,xlRange)*1000;
+c_AbsC_measured(isnan(c_AbsC_measured))=0;
+h_AbsC_measured=c_AbsC_measured/0.5;
+
     
 %Measured el price in NordPool in SEK/kWh
 sheet=2;
