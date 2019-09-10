bc444d309f9ef720e727532603677b5972bb418a
surferdavie <david.steen@chalmers.se>
2019-09-10 23:41:44 +0200

    added heating season for P2

--- FED_MAT_MAIN.m
+++ FED_MAT_MAIN.m
@@ -47,6 +47,7 @@ data_read_stop = sim_stop + forecast_horizon;
 DC_cooling_season_full = zeros(data_read_stop,1);
 no_imp_h_season_full = zeros(data_read_stop,1);
 DH_heating_season_full = ones(data_read_stop,1);
+DH_heating_season_P2_full = ones(data_read_stop,1);
 
 cooling_year = sim_start_y;
 while cooling_year <=sim_stop_y
@@ -65,6 +66,10 @@ while cooling_year <=sim_stop_y
     DC_cooling_season_full(cooling_season_start:cooling_season_end) = 1;
     no_imp_h_season_full(no_h_imp_season_start:no_h_imp_season_end) = 1;
     DH_heating_season_full(DH_heating_season_stop:DH_heating_season_start) = 0;
+    
+     DH_heating_season_P2_start = HoS(cooling_year,9,30,24);
+    DH_heating_season_P2_stop = HoS(cooling_year,5,1,1);
+    DH_heating_season_P2_full(DH_heating_season_stop:DH_heating_season_start) = 0;
     
     cooling_year = cooling_year + 1;
 end
@@ -626,6 +631,10 @@ for t=1:sim_length
     %DH heating season
     DH_heating_season = struct('name','DH_heating_season','type','parameter','form','full','val',DH_heating_season_full(t+sim_start-1:forecast_end+sim_start-1,:));
     DH_heating_season.uels=h.uels;
+   
+    %DH heating season P2
+    DH_heating_season_P2 = struct('name','DH_heating_season_P2','type','parameter','form','full','val',DH_heating_season_P2_full(t+sim_start-1:forecast_end+sim_start-1,:));
+    DH_heating_season_P2.uels=h.uels;
     
     %BAC saving period
     BAC_savings_period = struct('name','BAC_savings_period','type','parameter','form','full','val',BAC_savings_period_full(t+sim_start-1:forecast_end+sim_start-1,:));
@@ -755,7 +764,7 @@ for t=1:sim_length
         BID, BID_AH_el, BID_nonAH_el, BID_AH_h, BID_nonAH_h, BID_AH_c, BID_nonAH_c, BID_nonBTES,...
         el_demand, h_demand, c_demand, h_Boiler1_0, h_FlueGasCondenser1_0,...
         el_VKA1_0, el_VKA4_0, c_AbsC_0, G_roof, G_facade,...
-        BES_min_SoC, BTES_properties, BTES_model, P1P2_dispatchable, DH_heating_season,DC_cooling_season, no_imp_h_season, BAC_savings_period,...
+        BES_min_SoC, BTES_properties, BTES_model, P1P2_dispatchable, DH_heating_season, DH_heating_season_P2, DC_cooling_season, no_imp_h_season, BAC_savings_period,...
         PVID, PVID_roof, PV_roof_cap, PVID_facade, PV_facade_cap,...
         el_price, el_certificate, tout, BAC_savings_factor, FED_Inv_lim, BusID,...
         opt_fx_inv_BTES_BAC_D_init, opt_fx_inv_BTES_BAC_S_init, opt_fx_inv_BTES_SO_D_init,...
