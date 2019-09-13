3f324af152a10177db2bce2f4c08048d2d994bc4
surferdavie <david.steen@chalmers.se>
2019-09-13 10:56:36 +0200

    update synt baseline
    Added data for PR4

--- RUN_FED_DS.m
+++ RUN_FED_DS.m
@@ -50,30 +50,30 @@ for i=7:10
     
     if i==7
         min_totCost_0=0;
-        min_totCost=0.8;
+        min_totCost=0.0001;
         min_totPE=0;
-        min_totCO2=0.2;
+        min_totCO2=1;
     end
      
     if i==8
         min_totCost_0=0;
-        min_totCost=0.6;
+        min_totCost=1;
         min_totPE=0;
-        min_totCO2=0.4;
+        min_totCO2=0.1;
     end
 
     if i==9
         min_totCost_0=0;
-        min_totCost=0.4;
+        min_totCost=1;
         min_totPE=0;
-        min_totCO2=0.6;
+        min_totCO2=0.001;
     end
     
     if i==10
         min_totCost_0=0;
-        min_totCost=0.2;
+        min_totCost=1;
         min_totPE=0;
-        min_totCO2=0.8;
+        min_totCO2=0.0001;
     end
 % Run FED model
 [to_excel_el, to_excel_heat, to_excel_cool, to_excel_co2, Results]=FED_MAT_MAIN(opt_RunGAMSModel, opt_marg_factors, min_totCost_0, min_totCost, min_totPE, min_totCO2, synth_baseline);
